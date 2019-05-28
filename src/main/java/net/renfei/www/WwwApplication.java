cb755ddf6c83b46dbbc1e164ced2caf5b14ba6a4
RenFei <i@renfei.net>
2019-05-28 14:12:10 +0800

    Initialize project structure

---
+++ src/main/java/net/renfei/www/WwwApplication.java
@@ -0,0 +1,13 @@
+package net.renfei.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwApplication.class, args);
+    }
+
+}
