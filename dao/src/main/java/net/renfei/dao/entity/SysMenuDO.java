7e4e26faea8bd7d6aec3098b9fd35cde876dd8c5
RenFei <i@renfei.net>
2019-09-03 14:57:42 +0800

    补充未上传的的文件

---
+++ dao/src/main/java/net/renfei/dao/entity/SysMenuDO.java
@@ -0,0 +1,33 @@
+package net.renfei.dao.entity;
+
+public class SysMenuDO {
+    private Long id;
+
+    private Long pid;
+
+    private Integer orderid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public Integer getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Integer orderid) {
+        this.orderid = orderid;
+    }
+}
