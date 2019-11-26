138fbef5fa4ddfdd9f465e2ccc497bbf67e1f51b
RenFei <i@renfei.net>
2019-11-26 21:21:13 +0800

    新增UUID生成工具

--- web/src/main/java/net/renfei/web/controller/KitboxController.java
+++ web/src/main/java/net/renfei/web/controller/KitboxController.java
@@ -20,6 +20,8 @@ public class KitboxController extends BaseController {
                 "IP地址信息查询工具，开放服务接口实现IP信息查询", domain + "/kitbox/ip"));
         kitboxVOList.add(new KitboxVO("fa-globe", "Dig+trace命令工具",
                 "域名解析查询工具，开放服务接口实现dig+trace查询", domain + "/kitbox/digtrace"));
+        kitboxVOList.add(new KitboxVO("fa-globe", "UUID/GUID生成工具",
+                "在线批量生成 UUID/GUID 工具，支持大小写连词符", domain + "/kitbox/uuid"));
         mv.addObject("kitboxVOList", kitboxVOList);
         setHead(mv, "开发者工具箱 - Kitbox", "免费的开发者工具箱小工具，工欲善其事，必先利其器。");
         mv.setViewName("kitbox/index");
@@ -41,6 +43,13 @@ public class KitboxController extends BaseController {
     public ModelAndView getDigTrace(ModelAndView mv) {
         setHead(mv, "在线Dig+trace命令检测DNS状态工具 - 开发者工具箱 - Kitbox", "域名解析信息查询工具，开放服务接口实现dig+trace域名解析过程查询");
         mv.setViewName("kitbox/digtrace");
+        return mv;
+    }
+
+    @RequestMapping({"uuid", "guid", "UUID", "GUID"})
+    public ModelAndView getUUID(ModelAndView mv) {
+        setHead(mv, "UUID/GUID 在线批量生成工具 - 开发者工具箱 - Kitbox", "在线批量生成 UUID/GUID 工具");
+        mv.setViewName("kitbox/uuid");
         return mv;
     }
 }
